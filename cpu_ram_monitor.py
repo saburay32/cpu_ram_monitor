16a98eb26e0204bbf779156b34c095e951407679
saburay32 <saburay32@gmail.com>
2022-07-06 15:53:40 +0300

    создал форму до вида включительно

--- cpu_ram_monitor.py
+++ cpu_ram_monitor.py
@@ -24,10 +24,30 @@ class Application(tk.Tk):
         self.bar2 = ttk.LabelFrame(self, text='Manual')
         self.bar2.pack(fill=tk.X)
 
+        self.combo_win = ttk.Combobox(self.bar2, values=["hide", "don't hide", "min"],width=9,state='readonly')
+        self.combo_win.current(1)
+        self.combo_win.pack(side=tk.LEFT)
+
         ttk.Button(self.bar2, text='move').pack(side=tk.LEFT)
+        ttk.Button(self.bar2, text='>>>').pack(side=tk.LEFT)
+
+        self.bar2 = ttk.LabelFrame(self, text='Power')
+        self.bar2.pack(fill=tk.BOTH)
+
+        self.bind_class('Tk', '<Enter>', self.enter_mouse)
+        self.bind_class('Tk', '<Leave>', self.leave_mouse)
+
+    def enter_mouse(self, event):
+        if self.combo_win.current() == 0 or 1:
+            self.geometry('')
+
+    def leave_mouse(self, event):
+        if self.combo_win.current() == 0:
+            self.geometry(f'{self.winfo_width()}x1')
 
     def app_exit(self):
         self.destroy()
         sys.exit()
-        
-''''''
+
+root = Application()
+root.mainloop()
