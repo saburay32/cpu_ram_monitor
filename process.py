71d4265cb25b2dc0c45c178d630abbcac565b79a
saburay32 <saburay32@gmail.com>
2022-07-08 10:49:35 +0300

    добавил виджет обновления данных о состоянии потоков цпу

--- process.py
+++ process.py
@@ -1,9 +1,13 @@
 import psutil as pt
 
+
 class CpuBar:
     def __init__(self):
         self.cpu_count = pt.cpu_count(logical=False)
         self.cpu_count_logical = pt.cpu_count()
 
 
-CpuBar()
+    def cpu_percent_return(self):
+        return pt.cpu_percent(percpu=True)
+
+
